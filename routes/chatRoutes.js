1d7eb4a91f640aeca5ac1087c1fa90a703e47151
JulianaCarvalho16 <juliana.vivien@gmail.com>
2025-06-18 19:57:10 -0300

    atualização de ia utilizada pelo back

--- routes/chatRoutes.js
+++ routes/chatRoutes.js
@@ -2,35 +2,45 @@ import express from "express";
 import axios from "axios";
 import { db } from "../middlewere/authMiddlewere";
 import { autenticarToken } from "../middlewere/authMiddlewere";
+import { Timestamp } from "@google-cloud/firestore";
 
 const routes = express.Router();
+const huggingFaceApi = "https://api-inference.huggingface.co/models/EleutherAI/gpt-neo-1.3B"
+const huggingFaceToken = process.env.HUGGINGFACE_API_KEY;
 
-routes.post("/", autenticarToken, async(req, res) => {
-    const { mensagem, estilo, gostos} = req.body;
+routes.post("/", autenticarToken, async( req, res) => {
+    const { mensagem, estilo, gosto} = req.body;
     const userId = req.user.uid;
-
     try {
-        const rasaResponse = await axios.post("http://localhost:5005/webhooks/rest/webhook", {
-            sender: userId,
-            message: messagem
-        });
+        const prompt = `Usuario: ${mensagem} \nEstilo: ${estilo} \nGosto: ${gosto || "Nenhum informado"} \nChatbot`;
+    const hfResponse = await axios.post(
+        huggingFaceApi,
+        {inputs: prompt},
+        {
+            headers: {
+                Authorization: `Bearer ${huggingFaceToken}`,
+                "Content-Type": "application/json"
+            },
+        }
+    );
 
-        const resposta = rasaResponse.data?.[0]?.text || "Não entendi, poderia repetir?";
+    const resposta = hfResponse.data?.[0]?.generated_text?.replace(prompt, "") || "Descupa, não consegui entender!";
 
-        await db.collection("conversas").add({
-            userId,
-            mensagem,
-            resposta,
-            estilo,
-            gostos,
-            Timestamp: new Date(),
-        });
-
-        res.json({ resposta })
+    await db.collection("conversas").add({
+        userId,
+        mensagem,
+        resposta,
+        estilo,
+        gosto,
+        Timestamp: new Date(),
+        
+    })
+        res.json({ resposta });
     } catch (error){
-        console.error("Erro ao se comunicar com Rasa:", error.message);
-        res.status(500).json("Erro na resposta do chatbot");
+        console.error("Erro ao se comunicar com Hugging Face:", error.message);
+        res.status(500).json({ error: "Erro na resposta do chatbot."})
     }
-});
+})
+
 
 export default routes;
